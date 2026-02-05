95fa219f06d9b49051ec0e887a96d8d78bba0449
João Pedro <nmjoaopedro22@gmail.com>
2026-02-05 14:52:26 -0300

    feat: inicializa projeto

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+import clsx from "clsx/lite";
+
+const geistSans = Geist({
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: "umo",
+	description: "Plataforma para transcrição e resumo automático de reuniões.",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="pt-BR">
+			<body
+				className={clsx(
+					geistSans.variable,
+					geistMono.variable,
+					"antialiased",
+				)}
+			>
+				{children}
+			</body>
+		</html>
+	);
+}
